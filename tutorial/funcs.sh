0c6ae2e5752e96a666a64b8808c86830bb9d5c2e
diversemix <diversemix@gmail.com>
2022-01-17 14:52:27 +0000

    wip

--- tutorial/funcs.sh
+++ tutorial/funcs.sh
@@ -2,3 +2,20 @@ function wait() {
     echo -n "-------------------------- Press any key to continue -------------------------- "
     read any_old_char
 }
+
+function check_command() {
+    echo -n "💲 $* "
+    $* > /tmp/out && echo " ✅" || (echo " ❌"; exit 1)
+    cat /tmp/out
+}
+
+function check_long_command() {
+    echo "💲 $* "
+    $* 
+}
+
+function all_good() {
+    echo 
+    echo "                         🎉 All good, let's continue 🎉"
+    echo
+}
--- tutorial/step1.sh
+++ tutorial/step1.sh
@@ -1,13 +1,60 @@
 #!/bin/bash
 
+set -e
+
 source ./tutorial/funcs.sh
 
+# clear
+# cat ./tutorial/learn-kubernetes
+# cat ./tutorial/intro
+
+# wait
+
+# cat ./tutorial/starting
+
+# wait
+
+# clear
+# echo
+# echo docker ------------------------------------------------------------------------
+# check_command docker -v 
+# check_command docker ps 
+# echo
+# echo minikuke ----------------------------------------------------------------------
+# check_command minikube version
+# echo
+# echo kubectl -----------------------------------------------------------------------
+# check_command kubectl version --client --short=true
+# echo
+
+# all_good
+
+# wait
+
+# cat ./tutorial/minikube-start
+
+# wait
+
+# clear
+# check_long_command minikube start
+# echo
+# echo "Let's check the profile -------------------------------------------------------"
+# echo
+
+# check_command minikube profile list
+# check command kubectl cluster-info
+
+# all_good
+
+# wait
 clear
 cat ./tutorial/learn-kubernetes
-cat ./tutorial/intro
+cat ./tutorial/start-nginx
+export GREP_COLORS="ms=1;36"
+cat ./basic-nginx/nginx-pod.yml  | grep --color=always -E .*: 
+echo
 
 wait
-
-cat ./tutorial/starting
-
-wait
+clear
+pwd
+check_long_command kubectl create -f ./basic-nginx/nginx-pod.yml --save-config
